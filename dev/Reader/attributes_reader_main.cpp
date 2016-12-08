91ccfe0a1776d618fa8925cefdbef4da90cdeaf1
Nicholas Yue <yue.nicholas@gmail.com>
2016-12-08 15:26:53 -0800

    Fixed attribute name retrieval

--- dev/Reader/attributes_reader_main.cpp
+++ dev/Reader/attributes_reader_main.cpp
@@ -52,71 +52,17 @@ int main(int argc, char** argv)
 			{
 				if (!inputPointDataGrid->empty())
 				{
-					for (openvdb::points::PointDataTree::LeafCIter leafIter = inputPointDataGrid->tree().cbeginLeaf(); leafIter; ++leafIter)
+					if (!inputPointDataGrid->tree().empty())
 					{
-						const openvdb::points::AttributeSet& as = leafIter->attributeSet();
-						size_t num_attributes = as.size();
-						std::cout << boost::format("AttributeSet size %1%") % num_attributes << std::endl;
-						for (auto pos = 0;pos<num_attributes;pos++)
-						{
-							size_t descriptor_size = as.descriptor().size();
-							std::cout << boost::format("Descriptor name pair {%1%,%2%}") % as.descriptor().type(0).first.c_str() % as.descriptor().type(0).second.c_str() << std::endl;
-							std::cout << boost::format("descriptor_size %1%") % descriptor_size << std::endl;
-							const openvdb::points::AttributeArray* aa = as.get(pos);
-							if (aa)
-							{
-								std::cout << boost::format("Attribute name pair {%1%,%2%}") % aa->type().first.c_str() % aa->type().second.c_str() << std::endl;
-							}
+						openvdb::points::PointDataTree::LeafCIter leafIter = inputPointDataGrid->tree().cbeginLeaf();
+						const openvdb::points::AttributeSet::Descriptor& descriptor = leafIter->attributeSet().descriptor();
+						for (const auto& namePos : descriptor.map()) {
+						    const openvdb::Name& name = namePos.first;
+						    const size_t index = namePos.second;
+						    const openvdb::Name& valueType = descriptor.valueType(index);
+						    std::cout << boost::format("Attribute[%1%] is '%2%' of type %3%") % index % name.c_str() % valueType.c_str() << std::endl;
 						}
-						openvdb::points::AttributeHandle<openvdb::Vec3f>::Ptr handle = openvdb::points::AttributeHandle<openvdb::Vec3f>::create(leafIter->constAttributeArray("P"),true);
-						if (handle)
-							std::cout << "Handle is valid" << std::endl;
-						else
-							std::cout << "Handle is invalid" << std::endl;
 					}
-#ifdef UNUSED
-					std::cout << boost::format("PointDataGrid name = '%1%', type = '%2%', metadata count = %3%")
-									% inputPointDataGrid->getName()
-									% inputPointDataGrid->gridType()
-									% inputPointDataGrid->metaCount()
-									<< std::endl;
-
-					openvdb::MetaMap::ConstMetaIterator mIter = inputPointDataGrid->beginMeta();
-					openvdb::MetaMap::ConstMetaIterator mEIter = inputPointDataGrid->endMeta();
-
-					for (;mIter!=mEIter;++mIter)
-					{
-						std::cout << boost::format("MetaData['%1%'] has '%2%'") % mIter->first % mIter->second->str()<< std::endl;
-					}
-#endif // UNUSED
-
-#ifdef UNUSED
-					openvdb::points::PointDataTree& inputTree = inputPointDataGrid->tree();
-
-					const openvdb::points::PointDataGrid::TreeType::LeafNodeType& leaf = *inputTree.cbeginLeaf();
-		            const openvdb::points::AttributeSet& attributeSet = leaf.attributeSet();
-		            size_t num_attributes = attributeSet.size();
-					std::cout << boost::format("num_attributes = %1%") % num_attributes << std::endl;
-					for (size_t index=0;index<num_attributes;index++)
-					{
-						const openvdb::points::AttributeArray* aa = attributeSet.getConst(index);
-						std::cout << boost::format("Attribute[%1%] = %2%") % aa->type().first % aa->type().second << std::endl;
-					}
-
-
-		            const openvdb::points::AttributeSet::Descriptor& descriptor = leaf.attributeSet().descriptor();
-					std::cout << boost::format("descriptor.size() = %1%") % descriptor.size() << std::endl;
-					std::cout << boost::format("Meta Data count = %1%") % descriptor.getMetadata().metaCount() << std::endl;
-
-					openvdb::MetaMap::ConstMetaIterator mmIter = descriptor.getMetadata().beginMeta();
-					openvdb::MetaMap::ConstMetaIterator mmEIter = descriptor.getMetadata().endMeta();
-					size_t index = 0;
-					for (;mmIter!=mmEIter;++mmIter)
-					{
-						std::cout << boost::format("MetaData[%1%] = %2%") % index % mmIter->first << std::endl;
-						index++;
-					}
-#endif // UNUSED
 				}
 			}
 		}
